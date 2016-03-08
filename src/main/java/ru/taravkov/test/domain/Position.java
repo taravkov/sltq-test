d2798aac5ec3c1e62dd56eefa1090d1e56da4e8b
Vladislav Taravkov <vtaravkov@me.com>
2016-03-08 16:52:14 +0300

    getter setter position

--- src/main/java/ru/taravkov/test/domain/Position.java
+++ src/main/java/ru/taravkov/test/domain/Position.java
@@ -1,5 +1,9 @@
 package ru.taravkov.test.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,8 +13,13 @@ import javax.validation.constraints.NotNull;
  * Created by vladislav on 08.03.16.
  */
 @Entity
+@Getter @Setter @NoArgsConstructor
 public class Position {
     private @Id @GeneratedValue Long id;
 
     private @NotNull String name;
+
+    public Position(String name) {
+        this.name = name;
+    }
 }
