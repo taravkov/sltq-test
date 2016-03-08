7d712015491e6b6769b2cef24ac12242b05909dc
Vladislav Taravkov <vtaravkov@me.com>
2016-03-08 13:38:09 +0300

    initial commit

---
+++ src/main/java/ru/taravkov/WebApplication.java
@@ -0,0 +1,15 @@
+package ru.taravkov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by vladislav on 08.03.16.
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
